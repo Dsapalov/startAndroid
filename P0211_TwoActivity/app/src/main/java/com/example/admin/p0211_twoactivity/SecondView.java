7c91b61c88d5c07369a9217071b0752cf14cfc01
Denis Sapalov <denis.sapalov@gmail.com>
2015-05-04 19:17:33 +0300

    Two activity

---
+++ P0211_TwoActivity/app/src/main/java/com/example/admin/p0211_twoactivity/SecondView.java
@@ -0,0 +1,19 @@
+package com.example.admin.p0211_twoactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.admin.p0211_twoactivity.R;
+
+/**
+ * Created by admin on 04.05.2015.
+ */
+public class SecondView extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s);
+    }
+
+}
